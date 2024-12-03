6ed785ef990e97d9702d13834fd3ba4a1586b7c8
Aydom Aparecido dos Santos <101830296+AydomSantos@users.noreply.github.com>
2024-12-03 17:32:03 -0300

    Update index.mjs

--- escola-api/src/routes/index.mjs
+++ escola-api/src/routes/index.mjs
@@ -1,16 +1,19 @@
 
 
-// Importar rotas de alunos e professores
+
+// Importar as rotas para os alunos, professores e turmas
 
 import {Router} from 'express';
 import alunosRoutes from './aluno.mjs';
 import ProfessoresRouter from './professor.mjs';
+import TurmaRouter from './turma';
 
 // Criar a rota raíz para o Express
 const router = Router();
 
-// Utilizar rotas para alunos e professores
+// Usar as rotas criadas para os alunos, professores e turmas
 router.use('/alunos', alunosRoutes);
 router.use('/professores', ProfessoresRouter);
+router.use('/truma', TurmaRouter);
 
 export default router;
