e06d75efe6d606154160ee0978e038dcb8918e2c
Aydom Aparecido dos Santos <101830296+AydomSantos@users.noreply.github.com>
2024-12-02 21:33:01 -0300

    Update professor.mjs

--- escola-api/src/routes/professor.mjs
+++ escola-api/src/routes/professor.mjs
@@ -4,4 +4,8 @@ import ProfessoresController from '../controllers/ProfessoresController.mjs';
 const router = Router()
 
 router.get('/', ProfessoresController.index);
-router.post('/', ProfessoresController)
+router.post('/', ProfessoresController.armazenar);
+router.put('/:id', ProfessoresController.update);
+router.delete('/:id', ProfessoresController.delete);
+
+export default router;
